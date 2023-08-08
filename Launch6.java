1f386e102509e081ce7ab11f6daf7d4cc020b489
chaudhurisoumya <137050154+chaudhurisoumya@users.noreply.github.com>
2023-08-08 22:58:07 +0530

    Add files via upload

--- Launch6.java
+++ Launch6.java
@@ -1,16 +1,34 @@
-
+import java.util.*;
 public class Launch6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String s1="Telusko";
-	    String s2="iNeuron";
-		String s3="Telusko" + "iNeuron";
+		HashSet hs=new HashSet();
+		hs.add(100);
+		hs.add(50);
+		hs.add(150);
+		hs.add(25);
+		System.out.println(hs);
 		
-		String s4=s1+s2;
-		String s5=s1+"iNeuron";
-		System.out.println(s3==s4);
+		if(hs.contains(50))
+		{
+			System.out.println("In hashset 50 is present");
+		}
+		if(!hs.contains(30))
+		{
+			System.out.println("30 is not present");
+		}
+		
+		
+		LinkedHashSet lhs=new LinkedHashSet();
+		
+		lhs.add(100);
+		lhs.add(50);
+		lhs.add(150);
+		lhs.add(25);
+		System.out.println(lhs);
+		
 
 	}
 
